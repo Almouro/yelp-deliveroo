a3be7c2a825b22a87fe6845168fc0bc715d1228b
Almouro <contact@almouro.com>
2017-06-05 19:40:11 +0200

    Fix location extraction on new deliveroo page

--- src/deliveroo.jsx
+++ src/deliveroo.jsx
@@ -4,13 +4,20 @@ import ReactDOM from 'react-dom';
 import YelpApi from './yelp-api';
 import YelpRestaurantInformation from './YelpRestaurantInformation';
 
-YelpApi.setAuthenticationToken().then(() => {
-  const location = $('.restaurant-index-page--title').text();
+const getLocation = () => {
+  const urlSplit = window.location.pathname.split('/');
+  return urlSplit[urlSplit.length - 1];
+};
 
+YelpApi.setAuthenticationToken().then(() => {
   $('.restaurant-index-page-tile').each(function addYelpInfo() {
     const restaurantName = $(this).find('h3').text();
-    YelpApi.searchRestaurant(restaurantName, location)
+    YelpApi.searchRestaurant(restaurantName, getLocation())
     .then((restaurant) => {
+      if (!restaurant || restaurant.rating < 4) {
+        $(this).remove();
+        return;
+      }
       const yelpContent = window.document.createElement('div');
       $(this).append(yelpContent);
       ReactDOM.render(<YelpRestaurantInformation restaurant={restaurant} />, yelpContent);
